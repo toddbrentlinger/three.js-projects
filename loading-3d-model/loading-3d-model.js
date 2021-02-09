bc67fedf8d01fbe6807a1ee378fbf5e2c8c22f58
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-02-08 16:43:28 -0800

    Added Blender Suzanne monkey head model, directional light, and animation to loading-3d-model project

--- loading-3d-model/loading-3d-model.js
+++ loading-3d-model/loading-3d-model.js
@@ -1,42 +1,69 @@
 ﻿"use strict";
 
-//import * as THREE from 'three';
+//import * as THREE from '../node_modules/three/build/three.js';
+//import * as THREE from '../js/three.js';
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 // Scene/Camera/Renderer
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(
     75, // FOV (degrees)
     window.innerWidth / window.innerHeight, // aspect ratio
     0.1, // near clipping plane
     1000 // far clipping plane
 );
+camera.position.z = 3;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
-
 // Light
 
-const light = new THREE.AmbientLight(0x404040); // soft white light
-scene.add(light);
+// Ambient Light
+const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(ambientLight);
+
+// Directional Light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2.0);
+directionalLight.position.set(1, 1, 1);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
 
 // Load 3D Model
 
 const loader = new GLTFLoader();
-
-loader.load('Walther PPK Original.gltf', function (gltf) {
-    scene.add(gltf.scene);
-}, undefined, function (error) {
-    console.error(error);
-});
+let monkey;
+loader.load(
+    // resource URL - Walther PPK Original.gltf | monkey.glb
+    'monkey.glb',
+    // called when the resource is loaded
+    function (gltf) {
+        scene.add(gltf.scene);
+        monkey = gltf.scene.children[0];
+        console.log(monkey);
+        // Material
+        monkey.material.color = new THREE.Color(0xCD853F);
+        // Animate
+        animate();
+    },
+    // called while loading is progressing
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    // called when loading has errors
+    function (error) {
+        console.error(error);
+    }
+);
 
 // Render/Animate loop
 
 function animate() {
     requestAnimationFrame(animate);
+    monkey.rotation.x += 0.01;
+    monkey.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
-animate();
